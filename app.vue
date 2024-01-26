c741d7f01f576e60709764b3cfbe3e38d8f148cd
suqi <N18989364491@163.com>
2024-01-26 11:07:43 +0800

    nuxt初始化

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- Nuxt 基于文件的路由系统-->
+  <!-- 首页index/index.vue -->
+  <NuxtLink to="/" :style="{ 'margin-right': '10px' }">首页</NuxtLink>
+  <!-- 视频页video/video.vue -->
+  <NuxtLink to="/video">视频页</NuxtLink>
+  <!-- Nuxt页面路由，相当于vue中router-view -->
+  <NuxtPage />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // 是否开启服务端渲染
+  // ssr: false,
+  devtools: { enabled: true },
+});
